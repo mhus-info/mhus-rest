7906b9f3a54348af9f950100fa7ea8032c77c149
Mike Hummel <mike.hummel@billingsolutions.de>
2021-11-04 12:43:28 +0100

    Update JwtTokenNode.java

--- rest-core/src/main/java/de/mhus/rest/core/nodes/JwtTokenNode.java
+++ rest-core/src/main/java/de/mhus/rest/core/nodes/JwtTokenNode.java
@@ -17,10 +17,16 @@ package de.mhus.rest.core.nodes;
 
 import org.apache.shiro.subject.Subject;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import de.mhus.lib.annotations.service.ServiceComponent;
+import de.mhus.lib.core.MJson;
 import de.mhus.lib.core.MPeriod;
+import de.mhus.lib.core.MString;
 import de.mhus.lib.core.aaa.Aaa;
 import de.mhus.lib.core.aaa.BearerConfiguration;
+import de.mhus.lib.core.cfg.CfgString;
 import de.mhus.rest.core.CallContext;
 import de.mhus.rest.core.annotation.RestNode;
 import de.mhus.rest.core.api.Node;
@@ -32,6 +38,7 @@ import de.mhus.rest.core.result.JsonResult;
 @RestNode(parent = Node.ROOT_PARENT, name = "jwt_token")
 public class JwtTokenNode extends VoidNode {
 
+    private CfgString CFG_ROLE_FILTER = new CfgString(JwtTokenNode.class, "roleFilter", "");
     BearerConfiguration config = new BearerConfiguration(MPeriod.HOUR_IN_MILLISECOUNDS);
 
     @Override
@@ -46,6 +53,15 @@ public class JwtTokenNode extends VoidNode {
             result.createObjectNode().put("rc", -2);
             return;
         }
-        result.createObjectNode().put("token", token).put("rc", 0);
+        ObjectNode obj = result.createObjectNode();
+        obj.put("token", token).put("rc", 0);
+
+        if (MString.isSet(CFG_ROLE_FILTER.value())) {
+            ArrayNode roles = MJson.createArrayNode();
+            obj.set("roles", roles);
+            for (String role : Aaa.getRoles(Aaa.getSubject()))
+                if (role.matches(CFG_ROLE_FILTER.value()))
+                    roles.add(role);
+        }
     }
 }
